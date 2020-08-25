551034facc8d96deb9ef943c814b2255b958b726
André Mello <andremello91@gmail.com>
2020-08-24 21:19:07 -0400

    Changed time of cracking check.

--- Membrane/Membrane/DSFM.cs
+++ Membrane/Membrane/DSFM.cs
@@ -119,8 +119,12 @@ namespace RCMembrane
 		/// </summary>
 		private	void CalculateCrackSlip()
 		{
-			// Verify the initial crack angle
-			InitialCrackAngle();
+			// Verify if concrete is cracked
+			if (!Concrete.Cracked)
+				return;
+
+            // Verify the initial crack angle
+            InitialCrackAngle();
 
 			// Calculate crack local stresses
 			var vci = ShearAtCrack();
@@ -146,92 +150,88 @@ namespace RCMembrane
 			double theta1 = Concrete.PrincipalStrains.Theta1;
 
 			// Initiate stresses
-			double vci   = 0;
+			double vci = 0;
 
-			// Verify if concrete is cracked
-			if (Concrete.Cracked)
+            // Get the average strains
+            double
+                ex = AverageStrains.EpsilonX,
+				ey = AverageStrains.EpsilonY;
+
+			// Get concrete tensile stress
+			var fc1 = Concrete.PrincipalStresses.Sigma1;
+
+			// Get reinforcement ratio
+			double
+				psx = Reinforcement?.DirectionX?.Ratio ?? 0,
+				psy = Reinforcement?.DirectionY?.Ratio ?? 0;
+
+			// Get reinforcement angles
+			var (thetaNx, thetaNy) = Reinforcement?.Angles(theta1) ??
+			                         (Concrete.PrincipalStrains.Theta1,
+				                         Concrete.PrincipalStrains.Theta1 - Constants.PiOver2);
+
+			// Get reinforcement stresses
+			var rSt = Reinforcement?.Stresses;
+			double
+				fsx = rSt?.SigmaX ?? 0,
+				fsy = rSt?.SigmaY ?? 0;
+
+			// Calculate cosines and sines
+			//var (cosTheta, sinTheta) = Auxiliary.DirectionCosines(thetaC1);
+			var (cosNx, sinNx) = DirectionCosines(thetaNx);
+			var (cosNy, sinNy) = DirectionCosines(thetaNy);
+			double
+				//cosTheta2 = cosTheta * cosTheta,
+				//sinTheta2 = sinTheta * sinTheta,
+				cosNx2 = cosNx * cosNx,
+				cosNy2 = cosNy * cosNy;
+
+			// Solve the nonlinear equation by Brent Method
+			bool solution = false;
+			double de1Cr = 0;
+			try
 			{
-				// Get the average strains
-				double
-					ex = AverageStrains.EpsilonX,
-					ey = AverageStrains.EpsilonY;
+				solution = Brent.TryFindRoot(CrackEquilibrium, 0, 0.005, 1E-9, 100, out de1Cr);
 
-				// Get concrete tensile stress
-				var fc1 = Concrete.PrincipalStresses.Sigma1;
-
-				// Get reinforcement ratio
-				double
-					psx = Reinforcement?.DirectionX?.Ratio ?? 0,
-					psy = Reinforcement?.DirectionY?.Ratio ?? 0;
-
-				// Get reinforcement angles
-				var (thetaNx, thetaNy) = Reinforcement?.Angles(theta1) ??
-				                         (Concrete.PrincipalStrains.Theta1,
-					                         Concrete.PrincipalStrains.Theta1 - Constants.PiOver2);
-
-				// Get reinforcement stresses
-				var rSt = Reinforcement?.Stresses;
-				double
-					fsx = rSt?.SigmaX ?? 0,
-					fsy = rSt?.SigmaY ?? 0;
-
-				// Calculate cosines and sines
-				//var (cosTheta, sinTheta) = Auxiliary.DirectionCosines(thetaC1);
-				var (cosNx, sinNx) = DirectionCosines(thetaNx);
-				var (cosNy, sinNy) = DirectionCosines(thetaNy);
-				double
-					//cosTheta2 = cosTheta * cosTheta,
-					//sinTheta2 = sinTheta * sinTheta,
-					cosNx2 = cosNx * cosNx,
-					cosNy2 = cosNy * cosNy;
-
-				// Solve the nonlinear equation by Brent Method
-				bool solution = false;
-				double de1Cr = 0;
-				try
+				// Function to check equilibrium
+				double CrackEquilibrium(double de1CrIt)
 				{
-					solution = Brent.TryFindRoot(CrackEquilibrium, 0, 0.005, 1E-9, 100, out de1Cr);
+					// Calculate local strains
+					double
+						esCrxIt = ex + de1CrIt * cosNx2,
+						esCryIt = ey + de1CrIt * cosNy2;
 
-					// Function to check equilibrium
-					double CrackEquilibrium(double de1CrIt)
-					{
-						// Calculate local strains
-						double
-							esCrxIt = ex + de1CrIt * cosNx2,
-							esCryIt = ey + de1CrIt * cosNy2;
+					// Calculate reinforcement stresses
+					double
+						fscrxIt = Reinforcement?.DirectionX?.Steel.CalculateStress(esCrxIt) ?? 0,
+						fscryIt = Reinforcement?.DirectionY?.Steel.CalculateStress(esCryIt) ?? 0;
 
-						// Calculate reinforcement stresses
-						double
-							fscrxIt = Reinforcement?.DirectionX?.Steel.CalculateStress(esCrxIt) ?? 0,
-							fscryIt = Reinforcement?.DirectionY?.Steel.CalculateStress(esCryIt) ?? 0;
-
-						// Check equilibrium (must be zero)
-						return
-							psx * (fscrxIt - fsx) * cosNx2 + psy * (fscryIt - fsy) * cosNy2 - fc1;
-					}
+					// Check equilibrium (must be zero)
+					return
+						psx * (fscrxIt - fsx) * cosNx2 + psy * (fscryIt - fsy) * cosNy2 - fc1;
 				}
-				catch
+			}
+			catch
+			{
+				solution = false;
+			}
+			finally
+			{
+				// Verify if it reached convergence
+				if (solution)
 				{
-					solution = false;
-				}
-				finally
-				{
-					// Verify if it reached convergence
-					if (solution)
-					{
-						// Calculate local strains
-						double
-							esCrx = ex + de1Cr * cosNx2,
-							esCry = ey + de1Cr * cosNy2;
+					// Calculate local strains
+					double
+						esCrx = ex + de1Cr * cosNx2,
+						esCry = ey + de1Cr * cosNy2;
 
-						// Calculate reinforcement stresses
-						double
-							fscrx = Reinforcement?.DirectionX?.Steel.CalculateStress(esCrx) ?? 0,
-							fscry = Reinforcement?.DirectionY?.Steel.CalculateStress(esCry) ?? 0;
+					// Calculate reinforcement stresses
+					double
+						fscrx = Reinforcement?.DirectionX?.Steel.CalculateStress(esCrx) ?? 0,
+						fscry = Reinforcement?.DirectionY?.Steel.CalculateStress(esCry) ?? 0;
 
-						// Calculate shear stress
-						vci = psx * (fscrx - fsx) * cosNx * sinNx + psy * (fscry - fsy) * cosNy * sinNy;
-					}
+					// Calculate shear stress
+					vci = psx * (fscrx - fsx) * cosNx * sinNx + psy * (fscry - fsy) * cosNy * sinNy;
 				}
 			}
 
@@ -244,9 +244,6 @@ namespace RCMembrane
 		/// <param name="vci">Shear stress on crack surface, in MPa.</param>
 		private StrainState CrackSlip(double vci)
 		{
-			if (!Concrete.Cracked)
-				return StrainState.Zero;
-
 			// Get shear slip strains
 			double
 				ysa = StressCrackSlip(vci),
@@ -359,7 +356,7 @@ namespace RCMembrane
 
 			// Calculate shear slip strain by rotation lag approach
 			double
-				thetaIc = _thetaIc.Value,
+				thetaIc = _thetaIc ?? Concrete.PrincipalStrains.Theta1,
 				dThetaE = thetaE1 - thetaIc,
 				thetaL,
 				dThetaS;
